3ced08b26f23004b37ad7f29b30566c65f2a04fd
pedro23165 <xtpedrin1@gmail.com>
2024-11-04 00:12:14 -0300

    11111
    111111

--- login.js
+++ login.js
@@ -1,62 +1,17 @@
-let btn = document.querySelector('.fa-eye')
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
 
-btn.addEventListener('click', ()=>{
-  let inputSenha = document.querySelector('#senha')
-  
-  if(inputSenha.getAttribute('type') == 'password'){
-    inputSenha.setAttribute('type', 'text')
+  const phone = document.getElementById("loginPhone").value.trim();
+  const password = document.getElementById("loginPassword").value.trim();
+  const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+  const user = existingUsers.find(user => user.phone === phone && user.password === password);
+
+  if (user) {
+      alert("Login bem-sucedido!");
+      // Aqui você pode redirecionar para uma nova página ou realizar outra ação
+       window.location.href = 'pagina.html'; 
   } else {
-    inputSenha.setAttribute('type', 'password')
+      alert("Número de telefone ou senha incorretos.");
   }
-})
-
-function entrar(){
-  let usuario = document.querySelector('#usuario')
-  let userLabel = document.querySelector('#userLabel')
-  
-  let senha = document.querySelector('#senha')
-  let senhaLabel = document.querySelector('#senhaLabel')
-  
-  let msgError = document.querySelector('#msgError')
-  let listaUser = []
-  
-  let userValid = {
-    nome: null,
-    user: null,
-    senha: null
-  }
-  
-  listaUser = JSON.parse(localStorage.getItem('listaUser'))
-  
-  listaUser?.forEach((item) => {
-    if(usuario.value == item.userCad && senha.value == item.senhaCad){
-       
-      userValid = {
-         nome: item.nomeCad,
-         user: item.userCad,
-         senha: item.senhaCad
-       }
-      
-    }
-  })
-   
-  if(usuario.value == userValid.user && senha.value == userValid.senha){
-    window.location.href = './pagina.html'
-    
-    let mathRandom = Math.random().toString(16).substr(2)
-    let token = mathRandom + mathRandom
-    
-    localStorage.setItem('token', token)
-    localStorage.setItem('userLogado', JSON.stringify(userValid))
-  } else {
-    userLabel.setAttribute('style', 'color: red')
-    usuario.setAttribute('style', 'border-color: red')
-    senhaLabel.setAttribute('style', 'color: red')
-    senha.setAttribute('style', 'border-color: red')
-    msgError.setAttribute('style', 'display: block')
-    msgError.innerHTML = 'Usuário ou senha incorretos'
-    usuario.focus()
-  }
-  
-}
-
+});

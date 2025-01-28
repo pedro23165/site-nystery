493f4533bd7cdf22a2afd4a0c3377824f0a61baa
pedro23165 <xtpedrin1@gmail.com>
2025-01-28 15:21:52 -0400

    cu
    uuuuu

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -1,38 +1,46 @@
 const express = require('express');
 const path = require('path');
 
-
+// Configurações do servidor
 const endereco = 'localhost';
 const porta = 5000;
-
 
 function criarServidor() {
     const app = express();
 
- 
+    // Configuração da pasta estática (CSS, JS, imagens, etc.)
     app.use(express.static(path.join(__dirname, 'public')));
 
-    
+    // Rotas específicas para os arquivos HTML
     const rotas = [
         { caminho: '/', arquivo: 'html/index.html' },
         { caminho: '/login', arquivo: 'html/login.html' },
         { caminho: '/cadastro', arquivo: 'html/cadastro.html' },
         { caminho: '/pagina', arquivo: 'html/pagina.html' },
+        { caminho: '/reciclagem', arquivo: 'html/reciclagem.html' },
+        { caminho: '/sobre', arquivo: 'html/sobre.html' },
+        { caminho: '/relatorio', arquivo: 'html/relatorio.html' },
+        { caminho: '/instrucoes', arquivo: 'html/instrucoes.html' },
+        { caminho: '/faleconosco', arquivo: 'html/faleconosco.html' },
+        { caminho: '/doacoes', arquivo: 'html/doacoes.html' },
+        { caminho: '/calendario', arquivo: 'html/calendario.html' },
+        { caminho: '/cadastrovoluntario', arquivo: 'html/cadastrovoluntario.html' },
     ];
 
+    // Configuração de cada rota para servir os arquivos HTML
     rotas.forEach((rota) => {
         app.get(rota.caminho, (req, res) => {
             res.sendFile(path.join(__dirname, 'public', rota.arquivo));
         });
-    }); 
+    });
 
-  
-    app.listen(porta,endereco, () => {
+    // Inicialização do servidor
+    app.listen(porta, endereco, () => {
         console.log(`Servidor rodando em http://${endereco}:${porta}`);
     });
 
     return app;
 }
 
-
+// Inicializa o servidor
 criarServidor();
